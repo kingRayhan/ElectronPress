5dfb756d8dc60a3c17b4668061b9b1b858637e68
kingrayhan <rayhan095@gmail.com>
2017-01-10 11:35:56 +0600

    TGM plugin activator to library

--- lib/init.php
+++ lib/init.php
@@ -1,1 +1,11 @@
-<?php // silence is good
+<?php
+/**
+ * All third party libries 
+ */
+
+
+/**
+ * TGM plugin activator
+ */
+
+require_once dirname( __FILE__ ) . '/TGM-Plugin-Activation-2.6.1/required-plugins.php';
