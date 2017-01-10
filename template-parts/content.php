2dde85fb0f6c5feb86bf35b63aad3c24f9183710
King Rayhan <kingRayhan@users.noreply.github.com>
2017-01-10 09:26:58 +0600

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,7 +10,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry content'); ?>>
 	<header>
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php ELECTRON_THEME_SLUG_NAME_entry_meta(); ?>
